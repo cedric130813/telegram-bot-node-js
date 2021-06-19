a5773338d8f1b4e1742edc78e201c385122c019d
Ian Cedric Io <82087099+cedric130813@users.noreply.github.com>
2021-06-19 22:11:35 +0800

    Update telegram-bot.js

--- telegram-bot.js
+++ telegram-bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 
 // replace the value below with the Telegram token from BotFather
-const token = <TOKEN>;
+const token = TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
